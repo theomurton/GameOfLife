604cc6d57916b21ec15f2f18ad15cfa952dd9db9
Theodore Murton <theodoreharveymurton@gmail.com>
2023-02-21 16:36:27 +0000

    Working version, has intitial GUIS but no big gui.

--- Board.java
+++ Board.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Board implements Cloneable{
 	private int height;
 	private int width;
@@ -50,56 +49,3 @@ public class Board implements Cloneable{
 		}
 	}
 }
-=======
-public class Board implements Cloneable{
-	private int height;
-	private int width;
-	private boolean[][] board;
-	public void setHeight(int height){
-		this.height = height;
-	}
-	public void setArray(boolean[][] array){
-		this.board = array;
-	}
-	public void setWidth(int width){
-                this.width = width;
-        }
-	public int getHeight(){
-		return this.height;
-	}
-	public int getWidth(){
-                return this.width;
-        }
-	public boolean[][] getArray(){
-		return this.board;
-	}
-	public void setBoardSize(int height, int width){
-		this.board = new boolean[height][width];
-	}
-	public void printBoard(){
-		for (int i = 0; i < this.getHeight(); i++){
-			for(int j = 0; j < this.getWidth(); j++){
-				if (this.board[i][j]){
-					System.out.print(".");
-				} else {
-					System.out.print("O");
-				}
-			}
-			System.out.println("");
-		}
-	}
-	public Board getBoard(){
-		return this;
-	}
-	public boolean getIndex(int x, int y){
-		return this.board[x][y];
-	}
-	public void swapIndex(int y, int x){
-		if (this.board[y][x]){
-			this.board[y][x] = false;
-		} else {
-			this.board[y][x] = true;
-		}
-	}
-}
->>>>>>> 7959219407572fc9691f97410a419c65c00a6e6b
